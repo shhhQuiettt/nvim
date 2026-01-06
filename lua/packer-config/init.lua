2dc48a3368dcbae39432592b267c23aea4d4cbee
Krzysztof Skrobała <krzysztof.skro@gmail.com>
2026-01-06 20:00:58 +0100

    Change packer to lazy

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,211 +1,189 @@
-return require("packer").startup(function(use)
-    use({
-        "wbthomason/packer.nvim",
-        config = function()
-            vim.cmd([[
-                      augroup packer_user_config
-                        autocmd!
-                        autocmd BufWritePost */nvim/lua/packer-config/init.lua source <afile> | PackerCompile
-                        autocmd BufWritePost */nvim/lua/plugins/* source <afile> | PackerCompile
-                      augroup end
-                    ]])
-        end,
-    })
-    -- Let you sourand objects
-    use("tpope/vim-surround")
+-- Bootstrap lazy.nvim (automatically install if missing)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    -- Commenter
-    use("tpope/vim-commentary")
+-- Setup plugins
+require("lazy").setup({
+    -- Simple plugins
+    "tpope/vim-surround",
+    "tpope/vim-commentary",
+    "airblade/vim-gitgutter",
 
-
-    -- Fzf
-    use({
+    -- Fzf / Telescope
+    {
         "nvim-telescope/telescope.nvim",
-        requires = { { "nvim-lua/plenary.nvim" } },
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("plugins.telescope-c.config")
         end,
-    })
+    },
+
+    -- Autocompletion (CMP)
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            -- "quangnguyen30192/cmp-nvim-ultisnips",
+            -- "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            -- "onsails/lspkind.nvim",
+        },
+        config = function()
+            require("plugins.cmp-config.config")
+        end,
+    },
 
     -- LSP Config
-    use({
+    {
         "neovim/nvim-lspconfig",
+        dependencies = { "hrsh7th/nvim-cmp" }, -- Load after cmp
         config = function()
             require("plugins.lsp")
         end,
-        after = "nvim-cmp",
-    })
+    },
 
-    use({
-       "hrsh7th/nvim-cmp",
-       requires = {
-           {
-               "hrsh7th/cmp-nvim-lsp",
-               -- "quangnguyen30192/cmp-nvim-ultisnips",
-               -- 'saadparwaiz1/cmp_luasnip',
-               "hrsh7th/cmp-buffer",
-               "hrsh7th/cmp-path",
-               -- "onsails/lspkind.nvim",
-           },
-       },
-       config = function()
-           require("plugins.cmp-config.config")
-       end,
-    })
-
-    use { "catppuccin/nvim", 
-        as = "catppuccin" , 
+    -- Colorscheme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000, -- Ensure it loads first
         config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-    end  }
+            vim.cmd.colorscheme "catppuccin-mocha"
+        end,
+    },
 
+    -- Floaterm
+    {
+        "voldikss/vim-floaterm",
+        config = function()
+            require("plugins.floaterm.config")
+        end,
+    },
 
-   use({
-       "voldikss/vim-floaterm",
-       config = function()
-           require("plugins.floaterm.config")
-       end,
-   })
+    -- Copilot
+    {
+        "github/copilot.vim",
+        config = function()
+            require("plugins.copilot.config")
+        end,
+    },
 
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false,
+        config = function()
+            require("plugins.treesitter.config")
+        end,
+    },
 
-   -- Copilot
-   use({
-       "github/copilot.vim",
-       config = function()
-           require("plugins.copilot.config")
-       end,
-   })
+    -- {
+    --     "nvim-treesitter/nvim-treesitter-textobjects",
+    --     dependencies = { "nvim-treesitter/nvim-treesitter" },
+    -- },
 
+    -- Debugger (DAP)
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "mfussenegger/nvim-dap-python",
+        },
+        config = function()
+            require("plugins.dap.config")
+        end,
+    },
 
-   -- show git diffs
-   use({ "airblade/vim-gitgutter" })
+    -- R Language
+    {
+        "R-nvim/R.nvim",
+        ft = { "r", "rmd" },
+        config = function()
+            require("plugins.rnvim.config")
+        end,
+    },
 
+    -- -----------------------------------------------------------
+    -- Migrated Commented Plugins (Uncomment to enable)
+    -- -----------------------------------------------------------
 
+    -- {
+    --    "ellisonleao/gruvbox.nvim",
+    --    config = function()
+    --        -- vim.o.background = "light"
+    --        -- vim.cmd([[colorscheme gruvbox]])
+    --    end,
+    -- },
 
-   use({
-       "nvim-treesitter/nvim-treesitter",
-       run = ":TSUpdate",
-       config = function()
-           require("plugins.treesitter.config")
-       end,
-   })
+    -- {
+    --    "maxmellon/vim-jsx-pretty",
+    --    ft = { "jsx", "tsx", "js", "ts" },
+    --    config = function()
+    --        print("vim-jsx-pretty loaded")
+    --    end,
+    -- },
 
-   use({
-     "nvim-treesitter/nvim-treesitter-textobjects",
-     after = "nvim-treesitter",
-     requires = "nvim-treesitter/nvim-treesitter",
-   })
+    -- { "evanleck/vim-svelte", ft = { "svelte" } },
 
+    -- {
+    --    "jackguo380/vim-lsp-cxx-highlight",
+    --    ft = { "c", "cpp" },
+    -- },
 
+    -- {
+    --    "rcarriga/nvim-dap-ui",
+    --    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    --    config = function()
+    --        require("plugins.dap_ui.config")
+    --    end,
+    -- },
 
-   --debbuger
-   use({
-       "mfussenegger/nvim-dap",
-       config = function()
-           require("plugins.dap.config")
-       end,
+    -- {
+    --    "theHamsta/nvim-dap-virtual-text",
+    --    dependencies = { "mfussenegger/nvim-dap" },
+    --    config = function()
+    --        require("nvim-dap-virtual-text").setup()
+    --    end,
+    -- },
 
-       requires = {
-           "mfussenegger/nvim-dap-python",
-       },
-   })
+    -- {
+    --    "mfussenegger/nvim-jdtls",
+    --    dependencies = { "neovim/nvim-lspconfig", "mfussenegger/nvim-dap" },
+    --    ft = { "java" },
+    --    config = function()
+    --        require("plugins.jdtls.config")
+    --    end,
+    -- },
 
+    -- {
+    --    "nvim-lualine/lualine.nvim",
+    --    dependencies = { "nvim-tree/nvim-web-devicons" },
+    --    config = function()
+    --        require("lualine").setup()
+    --    end,
+    -- },
 
-   use({
-       "R-nvim/R.nvim",
-       config = function()
-           require("plugins.rnvim.config")
-       end,
-       ft = { "r", "rmd" },
-   })
+    -- {
+    --    "iamcco/markdown-preview.nvim",
+    --    build = function() vim.fn["mkdp#util#install"]() end,
+    --    ft = { "markdown" },
+    -- },
 
-end)
---    -- Colorscheme
---    use({
---        "ellisonleao/gruvbox.nvim",
---        config = function()
---            -- vim.o.background = "light"
---            -- vim.cmd([[colorscheme gruvbox]])
---        end,
---    })
-
---    -- Auto brackets closing
-
-
---    use({
---        "maxmellon/vim-jsx-pretty",
---        -- filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
---        ft = { "jsx", "tsx", "js", "ts" },
---        config = function()
---            print("vim-jsx-pretty loaded")
---        end,
---    })
-
---    use({ "evanleck/vim-svelte", ft = { "svelte" } })
-
---    use({
---        "jackguo380/vim-lsp-cxx-highlight",
---        -- config = function()
---        --     require("")
---        -- end,
---        ft = { "c", "cpp" },
---    })
-
-
---    --treesitter
-
-
-
---    use({
---        "rcarriga/nvim-dap-ui",
---        requires = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-
---        after = "nvim-dap",
---        config = function()
---            require("plugins.dap_ui.config")
---        end,
---    })
-
---    use({
---        "theHamsta/nvim-dap-virtual-text",
---        after = "nvim-dap",
---        config = function()
---            require("nvim-dap-virtual-text").setup()
---        end,
---    })
-
---    --java
---    use({
---        "mfussenegger/nvim-jdtls",
---        config = function()
---            require("plugins.jdtls.config")
---        end,
---        after = { "nvim-lspconfig", "nvim-dap" },
---        ft = { "java" },
---    })
-
---    -- line
---    use({
---        "nvim-lualine/lualine.nvim",
---        requires = { "nvim-tree/nvim-web-devicons", opt = true },
---        config = function()
---            require("lualine").setup()
---        end,
---    })
-
---    use({
---        "iamcco/markdown-preview.nvim",
---        run = function() vim.fn["mkdp#util#install"]() end,
---        ft = { "markdown" },
---    })
-
-
---    -- use({
---    --     "luk400/vim-jukit",
---    --     config = function()
---    --         require("plugins.jukit.config")
---    --     end,
---    -- })
---    -- end)
-
-
+    -- {
+    --     "luk400/vim-jukit",
+    --     config = function()
+    --         require("plugins.jukit.config")
+    --     end,
+    -- },
+})
